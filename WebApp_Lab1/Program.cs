9217cb75c2dd873e7a55fa3123a094fe5c1c2a04
gulchak-d <gulchak@knu.ua>
2026-02-05 17:32:47 +0200

    Lab 1 finish

---
+++ WebApp_Lab1/Program.cs
@@ -0,0 +1,28 @@
+using Azure.Identity;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddApplicationInsightsTelemetry();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
